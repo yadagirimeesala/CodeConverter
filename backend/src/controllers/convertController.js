917a6270ed10d6b310220486df78f40eaced8ed3
yadagirimeesala <22951A0239@iare.ac.in>
2025-07-22 22:24:33 +0530

    Updated code in backend

--- backend/src/controllers/convertController.js
+++ backend/src/controllers/convertController.js
@@ -1,16 +1,3 @@
-// const { callHuggingFace } =require('../services/huggingfaceService');
-
-// exports.convertCode = async(req,res) => {
-//     const { source_code , source_lang , target_lang } =req.body;
-//     try{
-//         const prompt=`Convert the following ${source_lang} code to ${target_lang}:\n\n${source_code}`;
-//         const output=await callHuggingFace(prompt);
-//         res.json({ output });
-//     }catch(err){
-//         res.status(500).json({error : 'Conversion failed', details:err.message});
-//     }
-// };
-
 const { callPerplexity } = require('../services/perplexityService');
 
 exports.convertCode = async (req, res) => {
