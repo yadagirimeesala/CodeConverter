39fffede17885a8a2f203322485fb0caab653e60
yadagirimeesala <22951A0239@iare.ac.in>
2025-07-23 21:05:10 +0530

    Updated frontend API URL

--- frontend/src/api/codeConverter.js
+++ frontend/src/api/codeConverter.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function convertCode({ source_code, source_lang, target_lang }) {
-  const response = await axios.post("http://localhost:5000/convert", {
+  const response = await axios.post("https://codeconverter-232y.onrender.com/convert", {
     source_code,
     source_lang,
     target_lang,
