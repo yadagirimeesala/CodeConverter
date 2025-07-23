d27b2aa59bc8e1e9f548b60f9055dd900ea9f3c1
yadagirimeesala <22951A0239@iare.ac.in>
2025-07-23 20:48:56 +0530

    Changes in package.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,4 +18,4 @@ app.get('/', (req, res) => {
 }); 
 
 const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=> console.log(`server is running on port ${PORT}`))
+app.listen(PORT,()=> console.log(`server is running on port http://localhost:${PORT}`))
